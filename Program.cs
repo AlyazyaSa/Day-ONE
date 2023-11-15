f25036098ed52e75b6f7244bff2c49b4dcde4afd
Alyazyiah Saif <93020498+AlyazyaSa@users.noreply.github.com>
2023-11-15 10:06:03 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace day_One
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, This is first code!");
+        }
+    }
+}
